b8f2ee714d065def3d50b00d176873eae3268803
Sydney Benson <syzzle@Syzzle-Pro.local>
2019-11-18 16:17:49 -0500

    Initial commit

---
+++ src/main/java/com/example/lesson_301/Lesson301Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson_301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson301Application.class, args);
+    }
+
+}
